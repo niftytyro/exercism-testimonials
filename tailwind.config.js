aaefb6e0a47a445cddf0e4006f9358f0a3ce21ae
Udasi Tharani <udasitharani@gmail.com>
2022-04-02 13:42:01 +0530

    setup tailwind and fonts

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+    },
+    colors: {
+      label: "#130B43",
+      "label-secondary": "#5C5589",
+      "default-base": "#3F3A5A",
+      10: "#3D3B45",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
